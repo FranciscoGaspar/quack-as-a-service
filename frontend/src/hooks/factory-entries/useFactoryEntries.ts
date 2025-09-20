7434e613be3dc0a7e37080fbb9fea723534cf165
Guido Pereira <stereopt@gmail.com>
2025-09-20 11:39:21 +0100

    feat: Data Table (#10)
    * feat: get from server
    
    * feat: data-table

--- frontend/src/hooks/factory-entries/useFactoryEntries.ts
+++ frontend/src/hooks/factory-entries/useFactoryEntries.ts
@@ -1,11 +1,12 @@
 import { KEYS } from "@/constants/queryKeys";
+import { GetFactoryEntries } from "@/services/factoryEntries.service";
 import { useQuery } from "@tanstack/react-query";
 
 export const useFactoryEntries = () => {
   return useQuery({
     queryKey: KEYS.factoryEntries,
     queryFn: () => {
-      return [];
+      return GetFactoryEntries();
     },
   });
 };
